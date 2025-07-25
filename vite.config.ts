1a916bc142c2648a1db24996880fab66d4cefc74
reikemdev <jimmymeneses11@gmail.com>
2025-07-25 12:28:56 -0400

    fix: update base URL in Vite configuration and restore postbuild script in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "ifis-redesign",
+  base: "/ifis-redesign/",
   plugins: [react(),],
   resolve: {
     alias: {
